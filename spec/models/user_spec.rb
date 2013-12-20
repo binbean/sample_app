30e25a0f2098b45d96e0bfffc1049394cd7a5f07
binbean <binbean1001@gmail.com>
2013-12-20 22:55:39 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,12 +14,23 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " "}
     it { should_not be_valid }
+  end
+
+  describe "when name is to long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
   describe "when email not present" do
@@ -30,11 +41,6 @@ describe User do
   describe "when email present" do
     before { @user.email = "cutemao@qq.com" }
     it { should be_valid }
-  end
-
-  describe "when name is to long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
